4eb6e29e2e7f86ba7aef15acc571e389915ada6d
Mike Brocchi <brocco@users.noreply.github.com>
2023-08-21 14:35:21 -0400

    testing

--- src/verify.ts
+++ src/verify.ts
@@ -5,7 +5,7 @@ console.log('hey world');
 
 const content = readFileSync(join(__dirname, 'foo.js'));
 
-if (content.toString() !== '// this is foo.js') {
+if (content.toString().indexOf('// this is foo.js') === -1) {
   throw new Error('Expected output not found')
 } else {
   console.log('Success!!!\nExpected output found')
